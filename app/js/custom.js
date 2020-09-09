4d88d395444c77f6fa45904e45c233ce3bf122d3
“briana <briana@thesistesging.com>
2020-09-08 21:43:44 -0400

    migrate old repo

---
+++ app/js/custom.js
@@ -0,0 +1,35 @@
+// text effect ---------------------------------------------------------------------------------
+const makeSpans = selector => {
+  const [...elements] = document.querySelectorAll(selector)
+  return elements.map(element => {
+    const text = element.innerText.split('')
+    const spans = text
+      .map(letter => '<span>' + letter + '</span>')
+      .join('')
+    return element.innerHTML = spans
+  })
+}
+
+// theme changer ---------------------------------------------------------------------------------
+function randomPageClass() {
+  const pageClasses = [
+    'pinks',
+    'orange-crush',
+    'blue-green',
+    'og',
+  ]
+
+  const randomIndex = Math.floor(Math.random() * pageClasses.length);
+  const randomClass = pageClasses[randomIndex];
+
+  console.log(randomClass)
+  document.body.classList.add(randomClass);
+}
+
+window.onload = function () {
+  makeSpans('p, h1, h2, h3')
+  // randomPageClass()
+}
+
+
+
